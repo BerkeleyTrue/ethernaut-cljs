6ea56bc96f12f68fd51b9338d0a549f1de7d6882
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-06-11 23:09:49 -0700

    feat: init design

--- src/app/layout.cljs
+++ src/app/layout.cljs
@@ -1,8 +1,33 @@
 (ns app.layout
-  (:require [helix.core :refer [defnc]]
-            [helix.dom :as d]))
+  (:require [helix.core :refer [defnc $]]
+            [helix.dom :as d]
+            [app.utils.core :refer [class-names]]
+            [app.side-bar]))
 
+
+(defnc Main []
+  (d/main
+    {:className
+     (class-names
+       :mt-12
+       :text-4xl
+       :flex-grow
+       :px-6)}
+
+    "Ethernauts"))
 
 (defnc App []
-  (d/div {:className "box-border container"}
-    "Hello World"))
+  (d/div
+    {:className
+     (class-names
+       :bg-gray-200
+       :flex
+       :flex-nowrap
+       :flex-col
+       :sm:flex-row
+       :h-screen
+       :overflow-hidden
+       :relative
+       :w-screen)}
+    ($ app.side-bar/SideBar)
+    ($ Main)))
---
+++ src/app/side_bar.cljs
@@ -0,0 +1,59 @@
+(ns app.side-bar
+  (:require [helix.core :refer [defnc $]]
+            [helix.dom :as d]
+            [app.utils.core :refer [class-names]]))
+
+(defnc NavItem []
+  (d/a
+    {:className
+     (class-names
+      :hover:text-gray-800
+      :hover:bg-gray-100
+      :flex
+      :items-center
+      :p-2
+      :my-6
+      :transition-colors
+      :dark:hover:text-white
+      :dark:hover:bg-gray-600
+      :duration-200
+      :text-gray-600
+      :dark:text-gray-400
+      :rounded-lg)}
+    (d/span
+      {:className
+       (class-names
+         :mx-4
+         :text-lg
+         :font-normal)}
+      "Coin Flip")
+    (d/span
+      {:className
+       (class-names
+         :flex-grow
+         :text-right)})))
+
+
+
+
+(defnc SideBar []
+  (d/aside
+    {:className
+     (class-names
+       :bg-gray-800
+       :flex-row
+       :justify-around
+       :h-40
+       :w-100
+       :sm:h-screen
+       :sm:w-72
+       :sm:flex-col
+       :sm:justify-around)}
+
+    (d/nav
+      {:className
+       (class-names
+         :mt-10
+         :px-6)}
+
+      ($ NavItem))))
