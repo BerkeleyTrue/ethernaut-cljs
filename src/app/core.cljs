b7c5e5e834d69a396ce8181ce7e2014d0435646d
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-06-17 10:31:56 -0700

    feat(app): preserve state between refresh

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -8,15 +8,18 @@
     [app.layout :as layout]
     [app.main.redux :as main-redux]))
 
+(defonce get-state (atom (constantly nil)))
 
 (defn ^:dev/after-load create-app
   ([] (create-app nil))
   ([default-state]
-   (let [store (create-store
+   (let [default-state (or (@get-state) default-state)
+         store (create-store
                  (verts/combine-reducers main-redux/reducer-slice)
                  default-state
                  (dev-tools-enhancer))]
 
+     (reset! get-state (:get-state store))
      (render ($ (.-Provider react-redux-context)
                 {:value store
                  ;; key to force re-renders on hot reload
