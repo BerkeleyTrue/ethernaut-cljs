f6ea48b2860568ca9c71c2bf6519763059a13c09
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-06-19 21:43:51 -0700

    fix(app/ui): use comp macro

--- src/app/ui/button.cljs
+++ src/app/ui/button.cljs
@@ -1,33 +1,33 @@
 (ns app.ui.button
-  (:require [helix.dom :as d]
+  (:require [helix.core :refer [defnc]]
+            [helix.dom :as d]
             [app.utils.core :refer [class-names]]))
 
-(defn button [props]
-  (let [children (.-children props)]
-
-    (d/button
-      {:class-name
-       (class-names
-         :px-6
-         :h-11
-         :text-base
-         :text-white
-         :transition
-         :ease-in
-         :duration-200
-         :font-semibold
-         :shadow-md
-         :flex
-         :flex-row
-         :items-center
-         :justify-center
-         :border-2
-         :border-white
-         :rounded-xl
-         :uppercase
-         :hover:text-gray-900
-         :hover:bg-white
-         :focus:outline-none
-         :focus:ring-2
-         :focus:ring-offset-4)}
-      (or children "Button"))))
+(defnc button [{:keys [children class-name]}]
+  (d/button
+    {:class-name
+     (class-names
+       class-name
+       :px-6
+       :h-11
+       :text-base
+       :text-white
+       :transition
+       :ease-in
+       :duration-200
+       :font-semibold
+       :shadow-md
+       :flex
+       :flex-row
+       :items-center
+       :justify-center
+       :border-2
+       :border-white
+       :rounded-xl
+       :uppercase
+       :hover:text-gray-900
+       :hover:bg-white
+       :focus:outline-none
+       :focus:ring-2
+       :focus:ring-offset-4)}
+    (or children "Button")))
