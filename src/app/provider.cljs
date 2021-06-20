e24e6da0d56dacba92991dd1b8f262c0346c37f9
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-06-20 16:58:07 -0700

    feat(app/provider): store chain

--- src/app/provider.cljs
+++ src/app/provider.cljs
@@ -1,13 +1,11 @@
 (ns app.provider
   (:require [redux.verticals :as verts]
             ["@metamask/detect-provider" :as detect-provider]
+            ["web3" :as web3]
             [app.redux :as app]))
 
 
-(defonce ^:private provider (atom nil))
-
-; (defn connect []
-;   (if (= @provider nil)))
+(defonce ^:private web3-ref (atom nil))
 
 (def ^:private chains
   {1 "mainnent"
@@ -21,10 +19,12 @@
 ; actions
 (def provider-detected (verts/create-action ::detected))
 (def connect-wallet (verts/create-action ::connect))
+(def chain-changed (verts/create-action ::chain-changed))
 
 (def default-state
   {:detected? false
    :run? false
+   :chainId nil
    :rinkeby? false})
 
 (def detected?-selector #(get-in % [::state :detected?]))
@@ -35,11 +35,19 @@
   {::state
    (verts/handle-actions
      {::detected
-      (fn [state {detected? :payload}]
+      (fn [state {{:keys [detected? chainId]} :payload}]
         (->
           state
           (assoc :detected? detected?)
-          (assoc :run? true)))}
+          (assoc :run? true)
+          (assoc :chainId chainId)))
+
+      ::chain-changed
+      (fn [state {chainId :payload}]
+        (->
+          state
+          (assoc :chainId chainId)))}
+
      default-state)})
 
 (defn provider-middleware
@@ -51,9 +59,12 @@
         (when (= (:type action) (get app/types :on-mount))
           (->
             (detect-provider)
-            (.then #(do
-                     (reset! provider %)
-                     (dispatch (provider-detected (boolean %)))))
+            (.then
+              (fn [^js/object provider]
+               (when provider
+                 (.on provider "chainChanged" (comp dispatch chain-changed))
+                 (dispatch (provider-detected {:detected? true})))))
+                 ; (when (not (.-chainId provider))))))
 
             (.catch (fn [err] (print err)))))
 
--- src/app/utils/core.cljs
+++ src/app/utils/core.cljs
@@ -1,7 +1,34 @@
 (ns app.utils.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [cljs-bean.core :refer [bean]]
+            ["invariant" :as invariant]))
 
-(defn ^:export class-names [& args]
+(defn over-args [f & transforms]
+  (invariant
+    (every? #(fn? %) transforms)
+    "over-args expects every transform to be a function but found %s" transforms)
+  (fn [& args]
+    (apply f (map #(%1 %2)
+               (concat transforms (repeat identity))
+               args))))
+
+(comment
+  (fn? (over-args * inc))
+  (= ((over-args identity inc) 1) 2)
+  (= ((over-args * inc #(* % 2)) 1 2) 8)
+  (= ((over-args vector #(* % 2) #(* % %)) 9 3) [81 6])
+  (try
+    (over-args inc 1)
+    (catch js/Error e
+      (instance? js/Error e))))
+
+(defn wrap-js-args [f]
+  (over-args f #(and % (bean %))))
+
+(comment
+  ((wrap-js-args identity) #js {"chainId" inc}))
+
+(defn class-names [& args]
   (str/join " "
     (mapv name
       (reduce
