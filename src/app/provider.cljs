e76849ca81aa42dcb8e1096436ed1d01170b9fea
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-06-20 18:43:18 -0700

    feat(app/provider): if no chain id make request

--- src/app/provider.cljs
+++ src/app/provider.cljs
@@ -5,7 +5,7 @@
             [app.redux :as app]))
 
 
-(defonce ^:private web3-ref (atom nil))
+(defonce ^:private provider-ref (atom nil))
 
 (def ^:private chains
   {1 "mainnent"
@@ -19,17 +19,20 @@
 ; actions
 (def provider-detected (verts/create-action ::detected))
 (def connect-wallet (verts/create-action ::connect))
-(def chain-changed (verts/create-action ::chain-changed))
+(def chain-changed (verts/create-action ::chain-changed #(if (string? %) (js/parseInt % 16) %)))
+(def chain-changed-error (verts/create-action ::chain-changed-error))
 
 (def default-state
   {:detected? false
    :run? false
-   :chainId nil
+   :chain-id nil
    :rinkeby? false})
 
+; selectors
 (def detected?-selector #(get-in % [::state :detected?]))
 (def run?-selector #(get-in % [::state :run?]))
 (def rinkeby?-selector #(get-in % [::state :rinkeby?]))
+(def chain-id-selector #(get-in % [::state :chain-id]))
 
 (def reducer-slicer
   {::state
@@ -40,32 +43,47 @@
           state
           (assoc :detected? detected?)
           (assoc :run? true)
-          (assoc :chainId chainId)))
+          (assoc :chain-id chainId)))
 
       ::chain-changed
       (fn [state {chainId :payload}]
         (->
           state
-          (assoc :chainId chainId)))}
+          (assoc :chain-id chainId)))}
 
      default-state)})
 
 (defn provider-middleware
-  [{:keys [dispatch]}]
+  [{:keys [dispatch get-state]}]
   (fn [next]
     (fn [action]
 
-      (let [result (next action)]
-        (when (= (:type action) (get app/types :on-mount))
+      (let [result (next action)
+            action-type (:type action)]
+
+        (when (= action-type (get app/types :on-mount))
           (->
             (detect-provider)
             (.then
               (fn [^js/object provider]
                (when provider
                  (.on provider "chainChanged" (comp dispatch chain-changed))
+                 (reset! provider-ref provider)
                  (dispatch (provider-detected {:detected? true})))))
-                 ; (when (not (.-chainId provider))))))
 
             (.catch (fn [err] (print err)))))
+
+        (when (= action-type ::detected)
+          (let [chain-id (chain-id-selector (get-state))
+                detected? (detected?-selector (get-state))
+                provider @provider-ref]
+
+            (when (and detected? (nil? chain-id))
+              (let [wb (web3. provider)]
+                (->
+                  (.getChainId (.-eth wb))
+                  (.then chain-changed)
+                  (.catch chain-changed-error)
+                  (.then dispatch))))))
 
         result))))
